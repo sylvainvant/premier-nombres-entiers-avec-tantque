a1825e7b716d88a804a32c27209ad29a5b9c8569
sylvainvant <sylvainvantieghem@gmail.com>
2018-12-24 15:22:04 +0100

    first commit

---
+++ premier nombres entiers avec tantque/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace premier_nombres_entiers_avec_tantque
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+//declaration variable
+double n;
+string saissie;
+double resultat = 0;
+double cpt = 1;
+    Console.WriteLine("entrez la valeur n a calculer:");
+    saissie = Console.ReadLine();
+    n = Convert.ToDouble(saissie);
+       while(cpt<=n)
+          {
+            resultat = resultat + cpt;
+            cpt = cpt + 1;
+          }
+       Console.WriteLine("Le resultat aver pour est:" + resultat);
+       // permet de conserver affichage consol
+       Console.ReadLine();
+        }
+    }
+}
